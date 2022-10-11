a6516c11626e1e81f4c41f24c2df1d84ba2c5c9c
dkrnjic <115499953+dkrnjic@users.noreply.github.com>
2022-10-11 08:45:10 +0200

    faw
    dw

---
+++ 02zadatak.js
@@ -0,0 +1,8 @@
+function primiString(text){
+let length = text.length;
+let half = length/2;
+let str1 = text.slice(half, text.lenght);
+console.log(str1);
+}
+let text = "Javascript i nije tako los";
+primiString(text);
---
+++ 03zadatak.js
@@ -0,0 +1,6 @@
+let array = [321,63,"Mermelada",4,"Kruh",1,31,"tanjur",50];
+function SortArray(arr){
+    console.log(...arr);
+  console.log(arr.sort());
+} 
+SortArray(array);
